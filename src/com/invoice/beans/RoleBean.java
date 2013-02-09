386c32ac1761012c931ebaff633e6055816a652c
Szymon <szymon.kamil@gmail.com>
2013-02-09 13:42:11 +0100

    RoleBean

---
+++ src/com/invoice/beans/RoleBean.java
@@ -0,0 +1,130 @@
+package com.invoice.beans;
+
+public class RoleBean {
+	private int idRole;
+	private String Name;
+	private boolean AddInvoice;
+	private boolean AddProduct;
+	private boolean AddReciept;
+	private boolean AddBuyInvoice;
+	private boolean ViewInvoice;
+	private boolean ViewWarehause;
+	private boolean ViewProduct;
+	private boolean ViewBuyInvoice;
+	private boolean ViewDelivery;
+	private boolean AddContractor;
+	private boolean ViewContractor;
+	private boolean AddUser;
+	private boolean ViewUser;
+	private boolean AddRole;
+	private boolean ViewRole;
+	private boolean ViewReciept;
+	public int getIdRole() {
+		return idRole;
+	}
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public boolean isAddInvoice() {
+		return AddInvoice;
+	}
+	public void setAddInvoice(boolean addInvoice) {
+		AddInvoice = addInvoice;
+	}
+	public boolean isAddProduct() {
+		return AddProduct;
+	}
+	public void setAddProduct(boolean addProduct) {
+		AddProduct = addProduct;
+	}
+	public boolean isAddReciept() {
+		return AddReciept;
+	}
+	public void setAddReciept(boolean addReciept) {
+		AddReciept = addReciept;
+	}
+	public boolean isAddBuyInvoice() {
+		return AddBuyInvoice;
+	}
+	public void setAddBuyInvoice(boolean addBuyInvoice) {
+		AddBuyInvoice = addBuyInvoice;
+	}
+	public boolean isViewInvoice() {
+		return ViewInvoice;
+	}
+	public void setViewInvoice(boolean viewInvoice) {
+		ViewInvoice = viewInvoice;
+	}
+	public boolean isViewWarehause() {
+		return ViewWarehause;
+	}
+	public void setViewWarehause(boolean viewWarehause) {
+		ViewWarehause = viewWarehause;
+	}
+	public boolean isViewProduct() {
+		return ViewProduct;
+	}
+	public void setViewProduct(boolean viewProduct) {
+		ViewProduct = viewProduct;
+	}
+	public boolean isViewBuyInvoice() {
+		return ViewBuyInvoice;
+	}
+	public void setViewBuyInvoice(boolean viewBuyInvoice) {
+		ViewBuyInvoice = viewBuyInvoice;
+	}
+	public boolean isViewDelivery() {
+		return ViewDelivery;
+	}
+	public void setViewDelivery(boolean viewDelivery) {
+		ViewDelivery = viewDelivery;
+	}
+	public boolean isAddContractor() {
+		return AddContractor;
+	}
+	public void setAddContractor(boolean addContractor) {
+		AddContractor = addContractor;
+	}
+	public boolean isViewContractor() {
+		return ViewContractor;
+	}
+	public void setViewContractor(boolean viewContractor) {
+		ViewContractor = viewContractor;
+	}
+	public boolean isAddUser() {
+		return AddUser;
+	}
+	public void setAddUser(boolean addUser) {
+		AddUser = addUser;
+	}
+	public boolean isViewUser() {
+		return ViewUser;
+	}
+	public void setViewUser(boolean viewUser) {
+		ViewUser = viewUser;
+	}
+	public boolean isAddRole() {
+		return AddRole;
+	}
+	public void setAddRole(boolean addRole) {
+		AddRole = addRole;
+	}
+	public boolean isViewRole() {
+		return ViewRole;
+	}
+	public void setViewRole(boolean viewRole) {
+		ViewRole = viewRole;
+	}
+	public boolean isViewReciept() {
+		return ViewReciept;
+	}
+	public void setViewReciept(boolean viewReciept) {
+		ViewReciept = viewReciept;
+	}
+}
