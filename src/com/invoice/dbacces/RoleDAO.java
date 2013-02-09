bef474d82036c614e58d218f03c84fb7806944ea
Szymon <szymon.kamil@gmail.com>
2013-02-09 13:58:17 +0100

    szkielet klasy RoleDaO

---
+++ src/com/invoice/dbacces/RoleDAO.java
@@ -0,0 +1,24 @@
+package com.invoice.dbacces;
+
+import java.util.List;
+
+import com.invoice.beans.RoleBean;
+
+public class RoleDAO {
+	public RoleBean getRole(int idRole)
+	{
+		return null;
+	}
+	public List<RoleBean> getRoles()
+	{
+		return null;
+	}
+	public boolean insertRole(RoleBean role)
+	{
+		return false;
+	}
+	public boolean updateRole(RoleBean role)
+	{
+		return false;
+	}
+}
