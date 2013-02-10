59c48daab6dca54e31e38e09e939707c3a263abe
Dawid <Dawid@Laptop-GohanDS>
2013-02-10 10:05:25 +0100

    DeliveryPositionBean with setter's and getter's

---
+++ src/com/invoice/beans/DeliveryPositionBean.java
@@ -0,0 +1,50 @@
+package com.invoice.beans;
+
+public class DeliveryPositionBean {
+	private int idProduct;
+	private int idExternalDelivery;
+	private float Count;
+	private float Price;
+	private String Unit;
+	
+	public void setIdProduct (int idProduct){
+		this.idProduct = idProduct;
+	}
+	
+	public int getIdProduct(){
+		return idProduct;
+	}
+	
+	public void setIdExternalDelivery(int idExternalDelivery){
+		this.idExternalDelivery = idExternalDelivery;
+	}
+	
+	public int getIdExternalDelivery(){
+		return idExternalDelivery;
+	}
+	
+	public void setCount(float Count){
+		this.Count = Count;
+	}
+	
+	public float getCount(){
+		return Count;
+	}
+	
+	public void setPrice(float Price){
+		this.Price = Price;
+	}
+	
+	public float getPrice(){
+		return Price;
+	}
+	
+	public void setUnit (String Unit){
+		this.Unit = Unit;
+	}
+	
+	public String getUnit (){
+		return Unit;
+	}
+	
+}
