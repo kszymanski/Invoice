609a18dae41da2c7603be47050c05d8b6b1077d3
Dawid <Dawid@Laptop-GohanDS>
2013-02-10 20:58:06 +0100

    TaxBean with setters and getters

---
+++ src/com/invoice/beans/TaxBean.java
@@ -0,0 +1,28 @@
+package com.invoice.beans;
+
+public class TaxBean {
+	private int idTax;
+	private String amount;
+	private float name;
+	
+	
+	public int getIdTax() {
+		return idTax;
+	}
+	public void setIdTax(int idTax) {
+		this.idTax = idTax;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public float getName() {
+		return name;
+	}
+	public void setName(float name) {
+		this.name = name;
+	}
+	
+}
