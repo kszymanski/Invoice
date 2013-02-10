358ca2b474c289cbcb304ea047090eb8da17a507
Dawid <Dawid@Laptop-GohanDS>
2013-02-10 22:34:57 +0100

    ContractorBean with setters and getters

---
+++ src/com/invoice/beans/ContractorBean.java
@@ -0,0 +1,85 @@
+package com.invoice.beans;
+
+public class ContractorBean {
+	private int idConstractor;
+	private String name;
+	private String longName;
+	private int nip;
+	private int regon;
+	private String street;
+	private String city;
+	private int postCode;
+	private String country;
+	private String region;
+	private char type;
+	
+	public int getIdConstractor() {
+		return idConstractor;
+	}
+	public void setIdConstractor(int idConstractor) {
+		this.idConstractor = idConstractor;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLongName() {
+		return longName;
+	}
+	public void setLongName(String longName) {
+		this.longName = longName;
+	}
+	public int getNip() {
+		return nip;
+	}
+	public void setNip(int nip) {
+		this.nip = nip;
+	}
+	public int getRegon() {
+		return regon;
+	}
+	public void setRegon(int regon) {
+		this.regon = regon;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(int postCode) {
+		this.postCode = postCode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public char getType() {
+		return type;
+	}
+	public void setType(char type) {
+		this.type = type;
+	}
+	
+	
+	
+}
