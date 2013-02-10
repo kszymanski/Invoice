3a32edc7fc3e36cf1866271ae3381a2bc57b9163
Dawid <Dawid@Laptop-GohanDS>
2013-02-10 10:32:48 +0100

    ProductBean with setter's and getter's

---
+++ src/com/invoice/beans/ProductBean.java
@@ -0,0 +1,79 @@
+package com.invoice.beans;
+
+public class ProductBean {
+
+	private int idProduct;
+	private String Name;
+	private String LongName;
+	private String Descryption;
+	private float DefaultPrice;
+	private float DefaultTax;
+	private int Code;
+	private String Picture;
+	
+	public void setIdProduct(int idProdcut){
+		this.idProduct = idProdcut;
+	}
+	
+	public int getIdProduct(){
+		return idProduct;
+	}
+	
+	public void setName(String Name){
+		this.Name = Name;
+	}
+	
+	public String getName(){
+		return Name;
+	}
+	
+	public void setLongName(String LongName){
+		this.LongName = LongName;
+	}
+	
+	public String getLongName(){
+		return LongName;
+	}
+	
+	public void setDescryption(String Descryption){
+		this.Descryption = Descryption;
+	}
+	
+	public String getDescryption(){
+		return Descryption;
+	}
+	
+	public void setDefaultPrice (float DefaultPrice){
+		this.DefaultPrice = DefaultPrice;
+	}
+	
+	public float getDefaultPrice(){
+		return DefaultPrice;
+	}
+	
+	public void setDefaultTax (float DefaultTax){
+		this.DefaultTax = DefaultTax;
+	}
+	
+	public float getDefaultTax(){
+		return DefaultTax;
+	}
+	
+	public void setCode(int Code){
+		this.Code = Code;
+	}
+	
+	public int getCode(){
+		return Code;
+	}
+	
+	public void setPicture(String Picture){
+		this.Picture = Picture;
+	}
+	
+	public String getPicture(){
+		return Picture;
+	}
+	
+	
+}
