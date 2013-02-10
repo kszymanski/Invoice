dd06c1c719790749c22dbcb94d6f548e9732e00d
Dawid <Dawid@Laptop-GohanDS>
2013-02-10 21:49:53 +0100

    BuyInvoiceBean getters and setters

---
+++ src/com/invoice/beans/BuyInvoiceBean.java
@@ -0,0 +1,23 @@
+package com.invoice.beans;
+
+public class BuyInvoiceBean {
+	private int idBuyInvoice;
+	private ExternalAdoptionBean externalAdoption;
+	
+	public int getIdBuyInvoice() {
+		return idBuyInvoice;
+	}
+	
+	public void setIdBuyInvoice(int idBuyInvoice) {
+		this.idBuyInvoice = idBuyInvoice;
+	}
+	
+	public ExternalAdoptionBean getExternalAdoption() {
+		return externalAdoption;
+	}
+	
+	public void setExternalAdoption(ExternalAdoptionBean externalAdoption) {
+		this.externalAdoption = externalAdoption;
+	}
+	
+}
