db8df2d55b6f4106110b1ba3b1539e6ae72f1a4c
Dawid <Dawid@Laptop-GohanDS>
2013-02-16 15:05:52 +0100

    Szkielet klasy TaxDAO

---
+++ src/com/invoice/dbacces/TaxDAO.java
@@ -0,0 +1,35 @@
+package com.invoice.dbacces;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.invoice.beans.basic.TaxBean;
+
+public class TaxDAO {
+	public static TaxBean getTaxBean(int idTax){
+		ResultSet rs;
+		TaxBean tax = null;
+		String query="Select * From Tax Where idTax=?";
+		try {
+			PreparedStatement stm= DBCon.getConnection().prepareStatement(query);
+			stm.setInt(1, idTax);
+
+		// execute select SQL stetement
+		
+			rs = stm.executeQuery();
+			while (rs.next())
+			{
+				tax = new TaxBean();
+
+				
+			}
+			stm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return tax;
+		
+	}
+}
