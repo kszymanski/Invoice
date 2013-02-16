3b992489cafb44b559b30319b1584cbd74e65e28
Dawid <Dawid@Laptop-GohanDS>
2013-02-16 13:53:15 +0100

    ExternalDeliveryDAO

---
+++ src/com/invoice/dbacces/ExternalDeliveryDAO.java
@@ -0,0 +1,40 @@
+package com.invoice.dbacces;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.invoice.beans.basic.ExternalDeliveryBean;
+
+public class ExternalDeliveryDAO {
+	public static ExternalDeliveryBean getExternalDeliveryBean(int idExternalDelivery){
+		ResultSet rs;
+		ExternalDeliveryBean externaldelivery = null;
+		String query="Select * From ExternalDelivery Where idExternalDelivery=?";
+		try {
+			PreparedStatement stm= DBCon.getConnection().prepareStatement(query);
+			stm.setInt(1, idExternalDelivery);
+
+		// execute select SQL stetement
+		
+			rs = stm.executeQuery();
+			while (rs.next())
+			{
+				externaldelivery = new ExternalDeliveryBean();
+				externaldelivery.setIdExternalDelivery(rs.getInt("idReceipt"));
+			//	externaldelivery.setWarehause(WareHauseDAO.getWareHause(rs.getString("idWareHause");
+				externaldelivery.setAuto(rs.getBoolean("Auto"));
+				externaldelivery.setDate(rs.getDate("Date"));
+				externaldelivery.setIdContractor(rs.getInt("idContractor"));
+				externaldelivery.setIdUser(rs.getString("idUser"));
+				
+			}
+			stm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return externaldelivery;
+		
+	}
+}
